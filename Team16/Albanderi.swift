9c4a682978bd1008459d10e8f9196478be2f5390
Albanderi <Albanderi-bh@hotmail.com>
2023-10-08 12:31:34 +0300

    Albanderi

--- Team16/Albanderi.swift
+++ Team16/Albanderi.swift
@@ -1,18 +1,54 @@
 //
-//  Albanderi.swift
-//  Team16
+//  ContentView.swift
+//  Albanderi riyadh
 //
 //  Created by Albanderi Binhasher on 23/03/1445 AH.
 //
 
 import SwiftUI
 
+                
+            
 struct Albanderi: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Image("ddd")
+                .resizable(resizingMode: .stretch)
+                .frame(width: 300.0, height: 300.0)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.blue,lineWidth: 10))
+         
+            
+            Text(/*@START_MENU_TOKEN@*/"Albanderi Binhasher"/*@END_MENU_TOKEN@*/)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+                .multilineTextAlignment(.leading)
+                
+            
+            Text("Iam A translator sson to be (An App Develper) who is passionate about colors. it resebles life and joy to me. what is life without colors right?")
+                .font(.title2)
+                .foregroundColor(Color.blue)
+                .padding(.leading)
+            
+            
+            
+        
+                
+            
+            
+            
+            
+            
+            
+            
+         }
+        
     }
 }
 
 #Preview {
     Albanderi()
 }
+
