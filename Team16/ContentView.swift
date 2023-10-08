44b10d9cec6525cd4988a4720407f48ccef00412
shatha-an <shada-2013@hotmail.com>
2023-10-08 11:23:00 +0300

    Initial Commit

---
+++ Team16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team16
+//
+//  Created by Shatha Ajjaj on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Team16/Team16App.swift
@@ -0,0 +1,17 @@
+//
+//  Team16App.swift
+//  Team16
+//
+//  Created by Shatha Ajjaj on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Team16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
