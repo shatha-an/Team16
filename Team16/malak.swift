4952cb05ea45c30d5bac3b6e64c5fda9b043caaf
malakkariri <malakkariri@gmail.com>
2023-10-08 12:07:46 +0300

    malak

---
+++ Team16/malak.swift
@@ -0,0 +1,18 @@
+//
+//  malak.swift
+//  Team16
+//
+//  Created by malak kariri on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct malak: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    malak()
+}
