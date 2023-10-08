b35ef826427f4c3e2f05244c9c128dd436a439a6
Shaden <shadenalghamdi7@gmail.com>
2023-10-08 12:03:14 +0300

    Shaden

---
+++ Team16/Shaden.swift
@@ -0,0 +1,18 @@
+//
+//  Shaden.swift
+//  Team16
+//
+//  Created by Shaden Alghamdi on 08/10/2023.
+//
+
+import SwiftUI
+
+struct Shaden: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Shaden()
+}
