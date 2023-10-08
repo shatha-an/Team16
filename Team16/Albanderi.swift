e824be379ce2c3a930f48e65850a50ea2abc48c7
Albanderi <Albanderi-bh@hotmail.com>
2023-10-08 12:30:53 +0300

    Albanderi

---
+++ Team16/Albanderi.swift
@@ -0,0 +1,18 @@
+//
+//  Albanderi.swift
+//  Team16
+//
+//  Created by Albanderi Binhasher on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Albanderi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Albanderi()
+}
