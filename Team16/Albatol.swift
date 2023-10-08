f1c4b738e645123f0cd084dfedfedfbb8c1eb084
Albatol Almeshali <AlbatolAlmeshai@gmail.com>
2023-10-08 11:47:36 +0300

    Albatol

--- Team16/Albatol.swift
+++ Team16/Albatol.swift
@@ -1,15 +1,46 @@
 //
-//  Albatol.swift
-//  Team16
+//  ContentView.swift
+//  Test
 //
-//  Created by Albatol Almeshali on 23/03/1445 AH.
+//  Created by Albatol Almeshali on 20/03/1445 AH.
 //
 
 import SwiftUI
 
 struct Albatol: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      
+        VStack () {
+    
+            Image("Mira")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+              
+               
+                .background(
+                    Circle()
+                        .fill(Color.yellow)
+                        .frame(width: 215, height: 215)
+                    
+                )
+            Text("Albatol Almeshali")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color("Dark green"))
+                .multilineTextAlignment(.leading)
+                .padding(.leading, -80)
+            
+            Text("Enjoy learning everything that is interesting to me, love my cat and love animals of all kinds, Always excited when doing what I do best and enjoy it")
+                .font(.callout)
+                .multilineTextAlignment(.leading)
+                .padding(.leading)
+            
+              
+
+        }
+        
+        .padding()
     }
 }
 
