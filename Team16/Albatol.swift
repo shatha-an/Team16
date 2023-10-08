96f87d304a6cbd99eb851b2c1706bdd868a91e07
Albatol Almeshali <AlbatolAlmeshai@gmail.com>
2023-10-08 11:47:09 +0300

    Albatol

---
+++ Team16/Albatol.swift
@@ -0,0 +1,18 @@
+//
+//  Albatol.swift
+//  Team16
+//
+//  Created by Albatol Almeshali on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Albatol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Albatol()
+}
